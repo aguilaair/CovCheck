f5764bcbd509022099f4598a8694b8698e98af8b
Eduardo <eduardomoreno.eduardo@gmail.com>
2022-01-01 12:00:24 +0100

    Add utility functions

--- lib/models/settings.dart
+++ lib/models/settings.dart
@@ -1,6 +1,6 @@
-import 'package:covid_checker/generated/l10n.dart';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:honeywell_scanner/honeywell_scanner.dart';
 
 class Settings {
@@ -11,8 +11,8 @@ class Settings {
 
   Settings({
     required this.isPda,
-    required this.isDarkMode,
     required this.isPdaModeEnabled,
+    required this.isDarkMode,
     required this.locale,
   });
 
@@ -27,5 +27,66 @@ class Settings {
       isPdaModeEnabled: isPdaModeEnabled,
       locale: locale.languageCode,
     );
+  }
+
+  Settings copyWith({
+    bool? isPda,
+    bool? isPdaModeEnabled,
+    bool? isDarkMode,
+    String? locale,
+  }) {
+    return Settings(
+      isPda: isPda ?? this.isPda,
+      isPdaModeEnabled: isPdaModeEnabled ?? this.isPdaModeEnabled,
+      isDarkMode: isDarkMode ?? this.isDarkMode,
+      locale: locale ?? this.locale,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'isPda': isPda,
+      'isPdaModeEnabled': isPdaModeEnabled,
+      'isDarkMode': isDarkMode,
+      'locale': locale,
+    };
+  }
+
+  factory Settings.fromMap(Map<String, dynamic> map) {
+    return Settings(
+      isPda: map['isPda'] ?? false,
+      isPdaModeEnabled: map['isPdaModeEnabled'] ?? false,
+      isDarkMode: map['isDarkMode'] ?? false,
+      locale: map['locale'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Settings.fromJson(String source) =>
+      Settings.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Settings(isPda: $isPda, isPdaModeEnabled: $isPdaModeEnabled, isDarkMode: $isDarkMode, locale: $locale)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Settings &&
+        other.isPda == isPda &&
+        other.isPdaModeEnabled == isPdaModeEnabled &&
+        other.isDarkMode == isDarkMode &&
+        other.locale == locale;
+  }
+
+  @override
+  int get hashCode {
+    return isPda.hashCode ^
+        isPdaModeEnabled.hashCode ^
+        isDarkMode.hashCode ^
+        locale.hashCode;
   }
 }
