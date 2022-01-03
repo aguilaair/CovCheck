3da3ada1d73efcd6d387de99f0e5460907841e8f
Eduardo M <contact@eduardom.dev>
2022-01-03 13:23:10 +0100

    Add willPopScope to stop closing app

--- lib/widgets/certOverview/result_card.dart
+++ lib/widgets/certOverview/result_card.dart
@@ -24,51 +24,57 @@ class ResultCard extends StatelessWidget {
     return ConstrainedBox(
       constraints:
           const BoxConstraints(maxHeight: 800, maxWidth: 800, minHeight: 300),
-      child: Dismissible(
-        direction: DismissDirection.down,
-        key: const Key("Dismissable"),
-        onDismissed: (direction) {
+      child: WillPopScope(
+        onWillPop: () async {
           dismiss();
+          return false;
         },
-        child: Container(
-          //width: double.infinity,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              OverallResult(
-                coseResult: coseResult,
-                processedResult: processedResult,
-              ),
-              if (processedResult != null)
-                Expanded(
-                  //height: 30,
-                  child: SingleChildScrollView(
-                    physics: const BouncingScrollPhysics(),
-                    child: CertInfoViewer(
-                      coseResult: coseResult,
-                      processedResult: processedResult!,
+        child: Dismissible(
+          direction: DismissDirection.down,
+          key: const Key("Dismissable"),
+          onDismissed: (direction) {
+            dismiss();
+          },
+          child: Container(
+            //width: double.infinity,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                OverallResult(
+                  coseResult: coseResult,
+                  processedResult: processedResult,
+                ),
+                if (processedResult != null)
+                  Expanded(
+                    //height: 30,
+                    child: SingleChildScrollView(
+                      physics: const BouncingScrollPhysics(),
+                      child: CertInfoViewer(
+                        coseResult: coseResult,
+                        processedResult: processedResult!,
+                      ),
                     ),
-                  ),
-                )
-            ],
-          ),
-          margin: const EdgeInsets.only(top: 20),
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(15),
-              topRight: Radius.circular(15),
+                  )
+              ],
             ),
-            boxShadow: [
-              BoxShadow(
-                color: Theme.of(context).brightness == Brightness.light
-                    ? Colors.grey
-                    : const Color(0xff27226A),
-                blurRadius: 15,
-                spreadRadius: -3,
-              )
-            ],
-            color: Theme.of(context).cardColor,
+            margin: const EdgeInsets.only(top: 20),
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(15),
+                topRight: Radius.circular(15),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Theme.of(context).brightness == Brightness.light
+                      ? Colors.grey
+                      : const Color(0xff27226A),
+                  blurRadius: 15,
+                  spreadRadius: -3,
+                )
+              ],
+              color: Theme.of(context).cardColor,
+            ),
           ),
         ),
       ),
